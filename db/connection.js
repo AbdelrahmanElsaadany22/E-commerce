d5772ad7966ce6b375d0840f8bf049a2aa76c489
Abdelrahman Elsayed <abooelsaadany@gmail.com>
2024-04-16 10:42:49 +0200

    day 2 commit

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const connectToDb = () => {
 	mongoose
 		.connect(process.env.DB_STRING)
-		.then(() => console.log('Connected to DB successfully...'))
+		.then(() => console.log('Connected to DB successfully in ',process.env.DB_STRING))
 		.catch(() => console.log('Error connecting to DB!'))
 }
 
